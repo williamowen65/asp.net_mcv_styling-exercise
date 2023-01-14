d9afe05d69a822b3eb70aa2e30b933febb5cf68f
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-01-13 16:18:51 -0800

    Add project files.

---
+++ ModelBindingStyling-Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelBindingStyling_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
