2f6c81e3f4eaa47e771346ed8265dbff6b216dc9
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-01-18 20:24:37 -0800

    Remove privacy page and references

--- ModelBindingStyling-Lab/Controllers/HomeController.cs
+++ ModelBindingStyling-Lab/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ModelBindingStyling_Lab.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
