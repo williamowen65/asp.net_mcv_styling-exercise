b0b2734ae30d69162bc05e5dc5419bbdb5d73c5e
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-01-18 20:56:37 -0800

    User profile page added

--- ModelBindingStyling-Lab/Controllers/HomeController.cs
+++ ModelBindingStyling-Lab/Controllers/HomeController.cs
@@ -18,6 +18,31 @@ namespace ModelBindingStyling_Lab.Controllers
             return View();
         }
 
+        public IActionResult UserProfile()
+        {
+            UserProfile user = GetUserProfileData();
+            return View(user);
+        }
+
+        /// <summary>
+        /// This method returns hardcoded data for a UserProfile
+        /// to use with modelbinding on a view
+        /// </summary>
+        private UserProfile GetUserProfileData()
+        {
+            return new UserProfile()
+            {
+                DateOfBirth = new DateOnly(1815, 7, 1),
+                Email = "First.Programmer@gmail.com",
+                FullName = "Ada Lovelace",
+                GitHubUrl = "https://github.com/Octocat",
+                ImageUrl = "https://via.placeholder.com/150",
+                PhoneNumber = "(253) 555-1234",
+                UserProfileId = 10,
+                SkilledLanguages = new List<string> { "C#", "Java", "C++" }
+            };
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ModelBindingStyling-Lab/Models/UserProfile.cs
@@ -0,0 +1,46 @@
+﻿namespace ModelBindingStyling_Lab.Models
+{
+    /// <summary>
+    /// Individual user profile information
+    /// </summary>
+    public class UserProfile
+    {
+        public int UserProfileId { get; set; }
+
+        /// <summary>
+        /// First and last legal name of the user
+        /// </summary>
+        public string FullName { get; set; } = null!;
+
+        /// <summary>
+        /// User date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Contact email
+        /// </summary>
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// Contact phone number
+        /// </summary>
+        public string PhoneNumber { get; set; } = null!;
+
+        /// <summary>
+        /// Profile image of the user
+        /// </summary>
+        public string ImageUrl { get; set; } = null!;
+
+        /// <summary>
+        /// The users GitHub profile URL
+        /// </summary>
+        public string GitHubUrl { get; set; } = null!;
+
+        /// <summary>
+        /// List of languages the user is skilled in. Will be an empty
+        /// list if there are 0
+        /// </summary>
+        public List<string> SkilledLanguages { get; set; } = new();
+    }
+}
