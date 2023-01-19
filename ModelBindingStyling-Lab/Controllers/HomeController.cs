b1df2241d5496f77caa3e077ab18fe753336adf0
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-01-18 21:15:11 -0800

    Printer list page added

--- ModelBindingStyling-Lab/Controllers/HomeController.cs
+++ ModelBindingStyling-Lab/Controllers/HomeController.cs
@@ -24,6 +24,52 @@ namespace ModelBindingStyling_Lab.Controllers
             return View(user);
         }
 
+        public IActionResult PrinterList()
+        {
+            IEnumerable<ThreeDPrinters> printers = GetPrinterList();
+            return View(printers);
+        }
+
+        /// <summary>
+        /// Returns a list of 3d printers with test data for use
+        /// on a view to practice modelbinding
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<ThreeDPrinters> GetPrinterList()
+        {
+            return new List<ThreeDPrinters>()
+            {
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "180 x 180 x 180 mm",
+                    Price = 499.99,
+                    SKU = "SMLPRINTER7",
+                    Title = "The mini printer"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "300 x 300 x 300 mm",
+                    Price = 899.99,
+                    SKU = "MEDPRINTER12",
+                    Title = "Mega Printer"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "360 x 360 x 360 mm",
+                    Price = 999.99,
+                    SKU = "MEDPRINTER12P",
+                    Title = "Mega Printer Plus"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "360 x 360 x 500 mm",
+                    Price = 899.99,
+                    SKU = "GIGANTOR10",
+                    Title = "Mega Printer"
+                }
+            };
+        }
+
         /// <summary>
         /// This method returns hardcoded data for a UserProfile
         /// to use with modelbinding on a view
---
+++ ModelBindingStyling-Lab/Models/ThreeDPrinters.cs
@@ -0,0 +1,34 @@
+﻿namespace ModelBindingStyling_Lab.Models
+{
+    /// <summary>
+    /// Represents an individual 3D printer
+    /// </summary>
+    public class ThreeDPrinters
+    {
+        /// <summary>
+        /// Stock Keeping Unit identifier
+        /// </summary>
+        public string SKU { get; set; } = null!;
+
+        /// <summary>
+        /// Title of the printer product
+        /// </summary>
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Sale price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Build volume that can be printed
+        /// ex. 180 x 180 x 180 mm
+        /// </summary>
+        public string BuildVolume { get; set; } = null!;
+
+        /// <summary>
+        /// Product image of the printer
+        /// </summary>
+        public string PrinterImage { get; set; } = "https://via.placeholder.com/300";
+    }
+}
