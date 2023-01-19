0c430c060702aef7ca8ad3768624746e8c6eb6b6
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-01-18 21:18:04 -0800

    Add display name attributes to models

--- ModelBindingStyling-Lab/Models/ThreeDPrinters.cs
+++ ModelBindingStyling-Lab/Models/ThreeDPrinters.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelBindingStyling_Lab.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingStyling_Lab.Models
 {
     /// <summary>
     /// Represents an individual 3D printer
@@ -18,17 +20,20 @@
         /// <summary>
         /// Sale price
         /// </summary>
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
 
         /// <summary>
         /// Build volume that can be printed
         /// ex. 180 x 180 x 180 mm
         /// </summary>
+        [Display(Name = "Build Volume")]
         public string BuildVolume { get; set; } = null!;
 
         /// <summary>
         /// Product image of the printer
         /// </summary>
+        [Display(Name = "Printer Image")]
         public string PrinterImage { get; set; } = "https://via.placeholder.com/300";
     }
 }
--- ModelBindingStyling-Lab/Models/UserProfile.cs
+++ ModelBindingStyling-Lab/Models/UserProfile.cs
@@ -1,4 +1,7 @@
-﻿namespace ModelBindingStyling_Lab.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ModelBindingStyling_Lab.Models
 {
     /// <summary>
     /// Individual user profile information
@@ -10,37 +13,44 @@
         /// <summary>
         /// First and last legal name of the user
         /// </summary>
+        [Display(Name = "Full Name")]
         public string FullName { get; set; } = null!;
 
         /// <summary>
         /// User date of birth
         /// </summary>
+        [Display(Name = "Date of birth")]
         public DateOnly DateOfBirth { get; set; }
 
         /// <summary>
         /// Contact email
         /// </summary>
+        [Display(Name = "Email Address")]
         public string Email { get; set; } = null!;
 
         /// <summary>
         /// Contact phone number
         /// </summary>
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; } = null!;
 
         /// <summary>
         /// Profile image of the user
         /// </summary>
+        [Display(Name = "Image URL")]
         public string ImageUrl { get; set; } = null!;
 
         /// <summary>
         /// The users GitHub profile URL
         /// </summary>
+        [Display(Name = "GitHub URL")]
         public string GitHubUrl { get; set; } = null!;
 
         /// <summary>
         /// List of languages the user is skilled in. Will be an empty
         /// list if there are 0
         /// </summary>
+        [Display(Name = "Skilled Languages")]
         public List<string> SkilledLanguages { get; set; } = new();
     }
 }
