85277ed33aaf3da38b69da160f27941a9c37c892
William Owen <william.owen.bsc@gmail.com>
2025-01-28 11:58:13 -0800

    Old WIP

--- ModelBindingStyling-Lab/Models/PrinterFilterViewModel.cs
+++ ModelBindingStyling-Lab/Models/PrinterFilterViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace ModelBindingStyling_Lab.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingStyling_Lab.Models
 {
     public class PrinterFilterViewModel
     {
         public string SKU { get; set; }
         public string Title { get; set; }
         public decimal? Price { get; set; }
+
+        [Display(Name ="Build Volume")]
         public string BuildVolume { get; set; }
 
+        [Display(Name ="Print Image")]
         public string PrinterImage { get; set; }
     }
 }
--- ModelBindingStyling-Lab/wwwroot/js/site.js
+++ ModelBindingStyling-Lab/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    //// Table respsonive listener
+    //const responsiveTable = document.querySelector("table.responsive");
+
+    //responsiveTable.addEventListener('')
+
+})
