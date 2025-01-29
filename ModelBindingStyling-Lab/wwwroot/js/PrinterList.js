b972d4da7dc50df89ca7bf750a26cbf7d7ff2cbd
William Owen <william.owen.bsc@gmail.com>
2025-01-29 10:31:27 -0800

    Add Odd/Even colors for the printer list

---
+++ ModelBindingStyling-Lab/wwwroot/js/PrinterList.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    // update the breadcrumb
+    Array.from(document.querySelectorAll('.breadcrumb-item'))
+        .forEach(el => {
+            el.classList.remove('active', 'disabled-link')
+        })
+    document.querySelector('#printer-list-breadcrumb').classList.remove('d-none')
+    document.querySelector('#printer-list-breadcrumb').classList.add('active', 'disabled-link')
+
+
+})
