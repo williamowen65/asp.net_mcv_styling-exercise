dafe1d8e117dc4ac146c6cee861cf87160089c14
William Owen <william.owen.bsc@gmail.com>
2025-01-29 09:44:26 -0800

    Overhaul the navbar with the breadcrumb component

---
+++ ModelBindingStyling-Lab/wwwroot/js/UserProfile.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    // update the breadcrumb
+    Array.from(document.querySelectorAll('.breadcrumb-item'))
+        .forEach(el => {
+            el.classList.remove('active', 'disabled-link')
+        })
+    document.querySelector('#user-profile-breadcrumb').classList.remove('d-none')
+    document.querySelector('#user-profile-breadcrumb').classList.add('active', 'disabled-link')
+
+
+})
